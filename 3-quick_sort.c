b621a4e662f0b7cbc9bbc3a10382c9541cb14230
jrichard1909 <jrichard1909@gmail.com>
2021-11-01 13:15:22 -0700

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,36 +8,41 @@
  * @size: size
  */
 
-void quick_sort2(int *array, size_t first, size_t last, size_t size)
+void quick_sort2(int *array, int first, int last, size_t size)
 {
-	size_t first_idx = first, last_idx = last, pivot = last, i = first;
-	int first_num = array[first], tmp;
+	int pivot_idx = last, i, j = first;
+	int pivot_num = array[last], tmp;
 
-	for (; i < last_idx; i++)
+	if (first >= last)
+		return;
+
+	for (i = first; i < last; i++)
 	{
-		if (first_num > array[pivot])
+		if (pivot_num > array[i])
 		{
-			tmp = array[first];
-			array[first] = array[pivot];
-			array[pivot] = tmp;
-			first++;
-			pivot = first;
-			print_array(array, size);
-		}
-		else if (first_num != array[pivot])
-		{
-			tmp = array[last];
-			array[last] = array[pivot];
-			array[pivot] = tmp;
-			last--;
-			pivot = last;
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+			j++;
 		}
 	}
 
-	if (pivot != 0)
-		quick_sort2(array, first_idx, pivot - 1, size);
-	if (pivot != last_idx)
-		quick_sort2(array, pivot + 1, last_idx, size);
+	if (i != j)
+	{
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+		print_array(array, size);
+	}
+
+	pivot_idx = j;
+
+	quick_sort2(array, first, pivot_idx - 1, size);
+	quick_sort2(array, pivot_idx + 1, last, size);
 }
 
 /**
